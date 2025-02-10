867339a2f802f59d18dfa66ff126f864620cc0bc
precalchard <seaphantom07@gmail.com>
2025-02-10 16:22:10 +0000

    Help idk how to code

--- Demo.py
+++ Demo.py
@@ -1,9 +1,6 @@
+print("I did task 2.")
 import pandas
 import matplotlib as plot
 
-data = pandas.read_csv("zooData.csv")
-
-plot.figure(figsize=(8,8))
-
-plot.plot(kind="bar", x=data["Animal"], y=data["Count"])
-
+data = pandas.Dataframe("zooData.csv")
+data.plot(x="")
