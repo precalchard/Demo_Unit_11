7e99edd4f01c6e593dc723d62f3e4512743e4160
precalchard <seaphantom07@gmail.com>
2025-02-11 02:14:16 +0000

    Completed demo

--- Demo.py
+++ Demo.py
@@ -1,6 +1,15 @@
 print("I did task 2.")
-import pandas
-import matplotlib as plot
 
-data = pandas.Dataframe("zooData.csv")
-data.plot(x="")
+import numpy as np
+import matplotlib.pyplot as plot
+
+x= np.array(["Croissant", "Danish", "Bear Claw", "Coffee Cake", "Cinnamon Roll"])
+y= np.array([4, 72, 23, 16, 91])
+plot.bar(x,y)
+
+plot.xlabel("Pastries")
+plot.ylabel("Amount")
+plot.title("Pastries in a café")
+
+plot.savefig("pastries_bar_graph.png")
+plot.show()
